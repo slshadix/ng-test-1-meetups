6fd76094f3d42e2bc8ee35f9b80eb6b33cb27876
Steve Shadix <slshadix@gmail.com>
2015-01-19 14:41:49 -0600

    initial version

---
+++ server/controllers/meetups-controller.js
@@ -0,0 +1,14 @@
+var Meetup = require('../models/meetups');
+
+module.exports.create = function (req, res) {
+	var meetup = new Meetup(req.body);
+	meetup.save(function (err, result) {
+		res.json(result);
+	});
+}
+
+module.exports.list = function (req, res) {
+	Meetup.find({}, function (err, results) {
+		res.json(results);
+	});
+}
